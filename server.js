769f606e24159067558b28e1ef2cd54f4d01c054
Carlos <carlos96831@gmail.com>
2022-01-01 18:55:36 -0800

    added info to server.js from mini project. wneeds changes

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
-//set up a route
+// Set up parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
+// set up a Route
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-//setup a listener
+// set up a Listener
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
